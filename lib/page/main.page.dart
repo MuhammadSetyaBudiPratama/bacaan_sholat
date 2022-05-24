b396c2bfe6fa04274d0c5ca4b254f40aec153955
MuhammadSetyaBudiPratama <msetyapratama27@gmail.com>
2022-05-24 08:34:15 +0700

    2

---
+++ lib/page/main.page.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+          child: Center(
+              child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            margin: const EdgeInsets.all(10),
+            child: Expanded(
+              child: InkWell(
+                highlightColor: Colors.transparent,
+                splashColor: Colors.transparent,
+                onTap: () {},
+                child: Column(
+                  children: const <Widget>[
+                    Image(
+                      image: AssetImage("assets/images/ic_niat.png"),
+                      height: 100,
+                      width: 100,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      "Niat Shalat",
+                      style:
+                          TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+          Container(
+            margin: const EdgeInsets.all(10),
+            child: Expanded(
+              child: InkWell(
+                highlightColor: Colors.transparent,
+                splashColor: Colors.transparent,
+                onTap: () {},
+                child: Column(
+                  children: const <Widget>[
+                    Image(
+                      image: AssetImage("assets/images/ic_doa.png"),
+                      height: 100,
+                      width: 100,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      "Bacaan Shalat",
+                      style:
+                          TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+          Container(
+            margin: const EdgeInsets.all(10),
+            child: Expanded(
+              child: InkWell(
+                highlightColor: Colors.transparent,
+                splashColor: Colors.transparent,
+                onTap: () {},
+                child: Column(
+                  children: const <Widget>[
+                    Image(
+                      image: AssetImage("assets/images/ic_bacaan.png"),
+                      height: 100,
+                      width: 100,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      "Ayat Kursi",
+                      style:
+                          TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ))),
+    );
+  }
+}
